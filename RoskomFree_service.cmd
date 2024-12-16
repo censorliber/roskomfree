05239625138c3491001926b9fb27e6edc6c3bfab
ЦензорНет <179885950+censorliber@users.noreply.github.com>
2024-12-16 19:10:33 +0300

    Add files via upload

---
+++ RoskomFree_service.cmd
@@ -0,0 +1,89 @@
+@echo off
+::::::::::::::::::::::::::::::::::::::::::::
+:: Elevate.cmd - Version 8
+:: Automatically check & get admin rights
+:: see "https://stackoverflow.com/a/12264592/1016343" for description
+::::::::::::::::::::::::::::::::::::::::::::
+ CLS
+ ECHO.
+ ECHO =============================
+ ECHO Running Admin shell
+ ECHO =============================
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~dpnx0"
+ rem this works also from cmd shell, other than %~0
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  whoami /groups /nh | find "S-1-16-12288" > nul
+  if '%errorlevel%' == '0' ( goto checkPrivileges2 ) else ( goto getPrivileges )
+
+
+:checkPrivileges2
+  net session 1>nul 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO.
+  ECHO **************************************
+  ECHO Invoking UAC for Privilege Escalation
+  ECHO **************************************
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+  
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+ ::::::::::::::::::::::::::::
+ ::START
+ ::::::::::::::::::::::::::::
+set "serviceName=Tor Win32 Service"
+sc query "%serviceName%" | find "RUNNING" >nul
+
+if %errorLevel% EQU 0 (
+   sc stop "Tor Win32 Service"
+   sc delete "Tor Win32 Service"
+   powershell -Command "(gc '%CD%\torrc.txt') "^
+  "-replace '^CacheDirectory.*$', ('CacheDirectory ..\data') "^
+  "-replace '^DataDirectory.*$', ('DataDirectory ..\data') "^
+  "-replace '^GeoIPFile.*$', ('GeoIPFile ..\data\geoip') "^
+  "-replace '^GeoIPv6File.*$', ('GeoIPv6File ..\data\geoip6') "^
+  "| Out-File -encoding ASCII '%CD%\torrc.txt'"
+) else (
+    powershell -Command "(gc '%CD%\torrc.txt') "^
+      "-replace '^CacheDirectory.*$', ('CacheDirectory \"%CD%\data\"' -replace '\\','\\') "^
+      "-replace '^DataDirectory.*$', ('DataDirectory \"%CD%\data\"' -replace '\\','\\') "^
+      "-replace '^GeoIPFile.*$', ('GeoIPFile \"%CD%\data\geoip\"' -replace '\\','\\') "^
+      "-replace '^GeoIPv6File.*$', ('GeoIPv6File \"%CD%\data\geoip6\"' -replace '\\','\\') "^
+      "| Out-File -encoding ASCII '%CD%\torrc.txt'"
+    sc create "Tor Win32 Service" binPath= "\"%CD%\tor\tor.exe\" --nt-service -f \"%CD%\torrc.txt\"" start= "auto"
+    sc description "Tor Win32 Service" "Provides an anonymous Internet communication system"
+    sc start "Tor Win32 Service"
+)
+
+pause
